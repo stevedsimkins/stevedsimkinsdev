a5d5550f0f497dd102ea5bdb24fb1c76078d0006
Steve <stevedsimkins@gmail.com>
2021-08-08 15:14:25 -0400

    small changes to bg, copyright, and mobile version

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,73 +2,67 @@ import PropTypes from 'prop-types'
 import React from 'react'
 
 const Header = props => {
-
-const blogLinkHandle = () => {
-  window.open("https://stevedsimkins.hashnode.dev", "_blank");
-};
-
-return (
-
-  <header id="header" style={props.timeout ? { display: 'none' } : {}}>
-    <div className="logo">
-      <span className="icon fa-code"></span>
-    </div>
-    <div className="content">
-      <div className="inner">
-        <h1>Steve Simkins</h1>
-        <p>
-          React Developer and Web Designer
-        </p>
-      </div>
-    </div>
-    <nav>
-      <ul>
-        <li>
-          <button
-            onClick={() => {
-              props.onOpenArticle('intro')
-            }}
-          >
-            Intro
-          </button>
-        </li>
-        <li>
-          <button
-            onClick={() => {
-              props.onOpenArticle('work')
-            }}
-          >
-            Work
-          </button>
-        </li>
-        <li>
-          <button
-            onClick={() => {
-              props.onOpenArticle('about')
-            }}
-          >
-            About
-          </button>
-        </li>
-                <li>
-          <button onClick={blogLinkHandle}>
-           Blog 
-          </button>
-        </li>
-        <li>
-          <button
-            onClick={() => {
-              props.onOpenArticle('contact')
-            }}
-          >
-            Contact
-          </button>
-        </li>
-      </ul>
-    </nav>
-  </header>
-)
+  const blogLinkHandle = () => {
+    window.open('https://stevedsimkins.hashnode.dev', '_blank')
   }
+
+  return (
+    <header id="header" style={props.timeout ? { display: 'none' } : {}}>
+      <div className="logo">
+        <span className="icon fa-code"></span>
+      </div>
+      <div className="content">
+        <div className="inner">
+          <h1>Steve Simkins</h1>
+          <p>Front-ed Web Developer</p>
+        </div>
+      </div>
+      <nav>
+        <ul>
+          <li>
+            <button
+              onClick={() => {
+                props.onOpenArticle('intro')
+              }}
+            >
+              Intro
+            </button>
+          </li>
+          <li>
+            <button
+              onClick={() => {
+                props.onOpenArticle('work')
+              }}
+            >
+              Work
+            </button>
+          </li>
+          <li>
+            <button
+              onClick={() => {
+                props.onOpenArticle('about')
+              }}
+            >
+              About
+            </button>
+          </li>
+          <li>
+            <button onClick={blogLinkHandle}>Blog</button>
+          </li>
+          <li>
+            <button
+              onClick={() => {
+                props.onOpenArticle('contact')
+              }}
+            >
+              Contact
+            </button>
+          </li>
+        </ul>
+      </nav>
+    </header>
+  )
+}
 
 Header.propTypes = {
   onOpenArticle: PropTypes.func,
